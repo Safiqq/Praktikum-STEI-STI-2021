c5f33215e8ba159833f0c980acf22107c059cae4
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2022-10-03 13:54:28 +0700

    upload praktikum 5 no. 3 (88)

---
+++ Algoritma dan Struktur Data STI/TP + Praktikum 5/cipher.c
@@ -0,0 +1,26 @@
+// NIM             : 18221048
+// Nama            : Syafiq Ziyadul Arifin
+// Tanggal         : 3 Oktober 2022
+// Topik praktikum : Praktikum 5
+// Deskripsi       : Program enkripsi dengan Caesar Cipher
+
+#include <stdio.h>
+#include "boolean.h"
+#include "mesinkarakterv2.h"
+#include "mesinkata.h"
+
+int main() {
+	STARTWORD();
+ 	int nFirstWord = currentWord.Length, i;
+	while (!isEndWord()) {
+		for (i = 0; i < currentWord.Length; i++) {
+			currentWord.TabWord[i] += nFirstWord;
+			if (currentWord.TabWord[i] > 'Z') currentWord.TabWord[i] -= 26;
+			printf("%c", currentWord.TabWord[i]);
+		}
+		ADVWORD();
+		if (!isEndWord() || currentWord.Length == 0) printf(" ");
+		else printf(".");
+	}
+	printf("\n");
+}
