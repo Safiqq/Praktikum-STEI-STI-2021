ef818be2eb48d258b9ec6dac3d8c10c01bcc667d
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2022-10-04 10:06:28 +0700

    add comment + beautify

--- Algoritma dan Struktur Data STI/TP + Praktikum 5/anagramalstrukdat.c
+++ Algoritma dan Struktur Data STI/TP + Praktikum 5/anagramalstrukdat.c
@@ -2,52 +2,60 @@
 // Nama            : Syafiq Ziyadul Arifin
 // Tanggal         : 3 Oktober 2022
 // Topik praktikum : Praktikum 5
-// Deskripsi       : 
+// Deskripsi       : Implementasi dari anagramalstrukdat.h
 
 #include <stdio.h>
 #include "anagramalstrukdat.h"
 
-int stringLength (char* string) {
+int stringLength(char *string)
+{
 	int count = 0;
-	while (*string != '\0') {
-		count++;
-		string++;
+	while (*string != '\0')
+	{
+		count++, string++;
 	}
 	return count;
 }
 
-Word toKata(char* command) {
+Word toKata(char *command)
+{
 	int i;
 	Word kata;
 	kata.Length = stringLength(command);
-	for (i = 0; i < kata.Length; i++) {
+	for (i = 0; i < kata.Length; i++)
+	{
 		kata.TabWord[i] = command[i];
 	}
 	return kata;
 }
 
-boolean isAnagram(Word string1, Word string2) {
+boolean isAnagram(Word string1, Word string2)
+{
 	int i;
 	int alfabet1[26] = {0}, alfabet2[26] = {0};
-	for (i = 0; i < stringLength(string1.TabWord); i++) {
-		if ('A' <= string1.TabWord[i] && string1.TabWord[i] <= 'Z') alfabet1[(int)string2.TabWord[i] - 65]++;
-		else if ('a' <= string1.TabWord[i] && string1.TabWord[i] <= 'z') alfabet1[(int)string1.TabWord[i] - 97]++;
+	for (i = 0; i < stringLength(string1.TabWord); i++)
+	{
+		if ('A' <= string1.TabWord[i] && string1.TabWord[i] <= 'Z') alfabet1[string2.TabWord[i] - 65]++;
+		else if ('a' <= string1.TabWord[i] && string1.TabWord[i] <= 'z') alfabet1[string1.TabWord[i] - 97]++;
 	}
-	for (i = 0; i < stringLength(string2.TabWord); i++) {
-		if ('A' <= string2.TabWord[i] && string2.TabWord[i] <= 'Z') alfabet2[(int)string2.TabWord[i] - 65]++;
-		else if ('a' <= string2.TabWord[i] && string2.TabWord[i] <= 'z') alfabet2[(int)string2.TabWord[i] - 97]++;
+	for (i = 0; i < stringLength(string2.TabWord); i++)
+	{
+		if ('A' <= string2.TabWord[i] && string2.TabWord[i] <= 'Z') alfabet2[string2.TabWord[i] - 65]++;
+		else if ('a' <= string2.TabWord[i] && string2.TabWord[i] <= 'z') alfabet2[string2.TabWord[i] - 97]++;
 	}
-	for (i = 0; i < 26; i++) {
+	for (i = 0; i < 26; i++)
+	{
 		if (alfabet1[i] != alfabet2[i]) return false;
 	}
 	return true;
-
 }
 
-void anagramalstrukdat(int* frek) {
+void anagramalstrukdat(int *frek)
+{
 	Word alstrukdat = toKata("alstrukdat");
 	STARTWORD();
-	while (!isEndWord()) {
+	while (!isEndWord())
+	{
 		*frek += isAnagram(alstrukdat, currentWord);
 		ADVWORD();
 	}
--- Algoritma dan Struktur Data STI/TP + Praktikum 5/hitungfrekuensi.c
+++ Algoritma dan Struktur Data STI/TP + Praktikum 5/hitungfrekuensi.c
@@ -2,29 +2,28 @@
 // Nama            : Syafiq Ziyadul Arifin
 // Tanggal         : 3 Oktober 2022
 // Topik praktikum : Praktikum 5
-// Deskripsi       : 
+// Deskripsi       : Implementasi dari hitungfrekuensi.h
 
 #include <stdio.h>
 #include "hitungfrekuensi.h"
 
-int hitungfrekuensi() {
+int hitungfrekuensi()
+{
 	int t = 0, i = 0, g = 0, a = 0, count = 0;
 	START();
-	while (GetCC() != MARK) {
+	while (GetCC() != MARK)
+	{
 		if (GetCC() == 't' || GetCC() == 'T') t = 1;
 		else if ((GetCC() == 'i' || GetCC() == 'I') && t == 1) i = 1;
 		else if ((GetCC() == 'g' || GetCC() == 'G') && t == 1 && i == 1) g = 1;
-		else if (GetCC() == 'a' || GetCC() == 'A') {
+		else if (GetCC() == 'a' || GetCC() == 'A')
+		{
 			if (t == 1 && i == 1 && g == 1) count++;
-			t = 0;
-			i = 0;
-			g = 0;
+			t = 0, i = 0, g = 0;
 		}
-		else {
-			t = 0;
-			i = 0;
-			g = 0;
-			a = 0;
+		else
+		{
+			t = 0, i = 0, g = 0, a = 0;
 		}
 		ADV();
 	}
