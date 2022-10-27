1aa1fa7a2283f1cf2847a73d9e0d3417b7bfc7b1
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2022-10-27 22:39:31 +0700

    upload no1 prak 7

---
+++ Algoritma dan Struktur Data STI/TP + Praktikum 7/browserhistory.c
@@ -0,0 +1,48 @@
+// NIM             : 18221048
+// Nama            : Syafiq Ziyadul Arifin
+// Tanggal         : 21 Oktober 2022
+// Topik praktikum : Pra-praktikum 7
+// Deskripsi       : Implementasi dari browserhistory.h
+
+#include <stdio.h>
+#include "browserhistory.h"
+
+void browserHistoryVisit(Stack *openedTab, Stack *history, char *url)
+{
+    Push(openedTab, url);
+    Push(history, url);
+}
+
+char *currentUrl(Stack *openedTab)
+{
+    return InfoTop(*openedTab);
+}
+
+char *browserHistoryBack(Stack *openedTab, Stack *history, int steps)
+{
+    int i;
+    infotype url;
+    for (i = 0; i < steps; i++)
+    {
+        Pop(openedTab, &url);
+        Push(history, url);
+    }
+    return currentUrl(openedTab);
+}
+
+char *browserHistoryForward(Stack *openedTab, Stack *history, int steps)
+{
+    int i;
+    infotype url;
+    for (i = 0; i < steps; i++)
+    {
+        Pop(history, &url);
+        Push(openedTab, url);
+    }
+    return currentUrl(openedTab);
+}
+
+void browserHistoryFree(Stack *history)
+{
+    CreateEmpty(history);
+}
